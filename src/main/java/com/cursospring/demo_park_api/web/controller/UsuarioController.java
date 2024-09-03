8742775229472fc293dce1807f1eeaf81a0e8c1f
jefersonoliveira <adv.jefersonoliveira@gmail.com>
2024-09-03 12:27:55 -0300

    Upd: Criado recurso de GET, POST, PATCH

--- src/main/java/com/cursospring/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/cursospring/demo_park_api/web/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package com.cursospring.demo_park_api.web.controller;
 
+import com.cursospring.demo_park_api.entity.Usuario;
 import com.cursospring.demo_park_api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@RequiredArgsConstructor - Lombok - inj. dep. será realizada via método construtor
 @RequiredArgsConstructor
@@ -13,4 +17,35 @@ public class UsuarioController {
 
     //injeção de dependências para service
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+
+        //resposta para o usuário:
+        //HttpStatus.CREATED - constante da spring
+        //.body() - inclui corpo da resposta que foi salvo no db.
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    //usamos patch para realizar alteração parcial, porém podemos usar @PutMapping tbm.
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    //listar todos usuários
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll() {
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
+
 }
